3c7a4a277435d6fdbb9650ce57496fa27c6cf1c2
Bradley Cushing <bradley.cushing@dashlane.com>
2019-10-06 17:11:54 -0400

    Add symlink.sh file for creating symlinks for main dot files

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# root level dotfile symbolic links
+ln -s ~/Documents/repos/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/Documents/repos/dotfiles/.gitconfig ~/.gitconfig
+
+# nvim config symbolic links
+ln -s ~/Documents/repos/dotfiles/.config/nvim/coc-config.vim ~/.config/nvim/coc-config.vim
+ln -s ~/Documents/repos/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -s ~/Documents/repos/dotfiles/.config/nvim/fugitive.vim ~/.config/nvim/fugitive.vim
+ln -s ~/Documents/repos/dotfiles/.config/nvim/fzf.vim ~/.config/nvim/fzf.vim
+ln -s ~/Documents/repos/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/Documents/repos/dotfiles/.config/nvim/mappings.vim ~/.config/nvim/mappings.vim

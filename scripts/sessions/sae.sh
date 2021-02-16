29662d7f857e771ea4c0583fdbc4399e7bac67e5
Bradley Cushing <bradleycushing@gmail.com>
2021-02-16 14:26:06 -0500

    feat(tmux): add extension project windows to sae session

--- scripts/sessions/sae.sh
+++ scripts/sessions/sae.sh
@@ -2,6 +2,7 @@
 
 SAE_DIRECTORY=~/Documents/repos/safariawakens
 LEELOO_DIRECTORY=~/Documents/repos/leeloo/packages/leeloo
+EXTENSION_DIRECTORY=~/Documents/repos/extension
 SESSION="sae"
 VIM="vim"
 
@@ -10,11 +11,17 @@ tmux has-session -t "$SESSION" 2>/dev/null
 # An error means there is no session
 if [[ $? != 0 ]]; then
   cd $SAE_DIRECTORY
+  # Create windows for safariawakens
   tmux new -s "$SESSION" -d
   tmux new-window -t "$SESSION"
   tmux send-keys -t "$SESSION" "$VIM" C-m
+  # Create windows for leeloo
   tmux new-window -t "$SESSION" -c $LEELOO_DIRECTORY
   tmux new-window -t "$SESSION" -c $LEELOO_DIRECTORY
+  tmux send-keys -t "$SESSION" "$VIM" C-m
+  # Create windows for extension
+  tmux new-window -t "$SESSION" -c $EXTENSION_DIRECTORY
+  tmux new-window -t "$SESSION" -c $EXTENSION_DIRECTORY
   tmux send-keys -t "$SESSION" "$VIM" C-m
 else
   echo "$SESSION already exists"

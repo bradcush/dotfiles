64ea0a73ab51d6b9a4ce0c46e29abd6c6aea15c9
Bradley Cushing <bradleycushing@gmail.com>
2021-02-16 14:25:27 -0500

    feat(tmux): add datalgo project windows to personal session

--- scripts/sessions/personal.sh
+++ scripts/sessions/personal.sh
@@ -2,6 +2,7 @@
 
 DOTFILES_DIRECTORY=~/Documents/repos/dotfiles
 MARK_DIRECTORY=~/Documents/repos/mark
+DATALGO_DIRECTORY=~/Documents/repos/datalgo
 SESSION="personal"
 VIM="vim"
 
@@ -10,11 +11,17 @@ tmux has-session -t "$SESSION" 2>/dev/null
 # An error means there is no session
 if [[ $? != 0 ]]; then
   cd $DOTFILES_DIRECTORY
+  # Create windows for dotfiles
   tmux new -s "$SESSION" -d
   tmux new-window -t "$SESSION"
   tmux send-keys -t "$SESSION" "$VIM" C-m
+  # Create windows for mark
   tmux new-window -t "$SESSION" -c $MARK_DIRECTORY
   tmux new-window -t "$SESSION" -c $MARK_DIRECTORY
+  tmux send-keys -t "$SESSION" "$VIM" C-m
+  # Create windows for datalgo
+  tmux new-window -t "$SESSION" -c $DATALGO_DIRECTORY
+  tmux new-window -t "$SESSION" -c $DATALGO_DIRECTORY
   tmux send-keys -t "$SESSION" "$VIM" C-m
 else
   echo "$SESSION already exists"

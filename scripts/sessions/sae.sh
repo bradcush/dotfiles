9626befd348c676860276cf33a39f1e478a73803
Bradley Cushing <bradleycushing@gmail.com>
2021-04-28 18:00:37 -0400

    feat(tmux): update safari tmux script with only safariawakens

--- scripts/sessions/sae.sh
+++ scripts/sessions/sae.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 SAE_DIRECTORY=~/Documents/repos/safariawakens
-LEELOO_DIRECTORY=~/Documents/repos/leeloo/packages/leeloo
-EXTENSION_DIRECTORY=~/Documents/repos/extension
 SESSION="sae"
 VIM="vim"
 
@@ -10,18 +8,10 @@ tmux has-session -t "$SESSION" 2>/dev/null
 
 # An error means there is no session
 if [[ $? != 0 ]]; then
-  cd $SAE_DIRECTORY
+  cd $SAE_DIRECTORY || exit
   # Create windows for safariawakens
   tmux new -s "$SESSION" -d
   tmux new-window -t "$SESSION"
-  tmux send-keys -t "$SESSION" "$VIM" C-m
-  # Create windows for leeloo
-  tmux new-window -t "$SESSION" -c $LEELOO_DIRECTORY
-  tmux new-window -t "$SESSION" -c $LEELOO_DIRECTORY
-  tmux send-keys -t "$SESSION" "$VIM" C-m
-  # Create windows for extension
-  tmux new-window -t "$SESSION" -c $EXTENSION_DIRECTORY
-  tmux new-window -t "$SESSION" -c $EXTENSION_DIRECTORY
   tmux send-keys -t "$SESSION" "$VIM" C-m
 else
   echo "$SESSION already exists"

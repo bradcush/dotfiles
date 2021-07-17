c1e2ac15e9b1141015cd4414f9c8c99572a5fbd4
Bradley Cushing <bradleycushing@gmail.com>
2021-07-17 18:49:47 -0400

    chore(install): replace vscode language servers with single extracted

--- scripts/install.sh
+++ scripts/install.sh
@@ -91,9 +91,8 @@ npm install -g bash-language-server
 npm install -g pyright
 npm install -g typescript typescript-language-server
 npm install -g vim-language-server
-npm install -g vscode-css-languageserver-bin
-npm install -g vscode-html-languageserver-bin
-npm install -g vscode-json-languageserver
+# Language servers for html, css, and json
+npm install -g vscode-langservers-extracted
 npm install -g yaml-language-server
 
 # Lua language server and formatter install

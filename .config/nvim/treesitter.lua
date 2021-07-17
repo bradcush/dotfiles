7af4cc87dd50abbf47c0b21864bd0d47a187f1ad
Bradley Cushing <bradleycushing@gmail.com>
2021-07-17 17:43:15 -0400

    feat(treesitter): enable treesitter syntax highlighting

--- .config/nvim/treesitter.lua
+++ .config/nvim/treesitter.lua
@@ -1,2 +1,20 @@
 -- Consistent syntax highlighting from tree-sitter
--- require'nvim-treesitter.configs'.setup {highlight = {enable = true}}
+require'nvim-treesitter.configs'.setup {
+    highlight = {enable = true},
+    ensure_installed = {
+        'bash',
+        'c',
+        'css',
+        'dockerfile',
+        'html',
+        'javascript',
+        'jsdoc',
+        'json',
+        'lua',
+        'python',
+        'toml',
+        'tsx',
+        'typescript',
+        'yaml'
+    }
+}

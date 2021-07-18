08aa3adbf4f81c4af6a3a0ea5a2cabd80c6f6ce5
Bradley Cushing <bradleycushing@gmail.com>
2021-07-18 11:38:24 -0400

    feat(treesitter): enable vim regex highlighting with treesitter

--- .config/nvim/treesitter.lua
+++ .config/nvim/treesitter.lua
@@ -1,6 +1,12 @@
 -- Consistent syntax highlighting from tree-sitter
 require'nvim-treesitter.configs'.setup {
-    highlight = {enable = true},
+    highlight = {
+        enable = true,
+        -- Running built-in syntax and treesiter at the same time for
+        -- less buggy spell check and bolding of return statements
+        -- Could slow down vim and create duplicate highlights
+        additional_vim_regex_highlighting = true
+    },
     ensure_installed = {
         'bash',
         'c',

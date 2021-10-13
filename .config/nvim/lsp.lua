fbf1f219c213545a72083608587141f889e85d8c
Bradley Cushing <bradleycushing@gmail.com>
2021-10-12 21:44:57 -0400

    feat(lsp): change diagnostic goto mapping to use mnemonic d

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -18,8 +18,8 @@ local on_attach = function(client, bufnr)
     keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     -- Disabling signature help as mapping conflicts with window navigation
     -- keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    keymap('n', '[g', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    keymap('n', ']g', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
     keymap('n', '<leader>dl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
            opts)
     keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)

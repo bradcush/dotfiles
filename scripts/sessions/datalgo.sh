080180977d4afffb90605a314b6b3906e4454f43
Bradley Cushing <bradleycushing@gmail.com>
2021-11-16 23:22:33 +0100

    feat(tmux): activate python environment for datalgo session

--- scripts/sessions/datalgo.sh
+++ scripts/sessions/datalgo.sh
@@ -2,6 +2,7 @@
 
 DATALGO_DIRECTORY=~/Documents/repos/datalgo
 SESSION="datalgo"
+PYENV="pyenv activate datalgo"
 VIM="vim"
 
 tmux has-session -t "$SESSION" 2>/dev/null
@@ -11,8 +12,9 @@ if [[ $? != 0 ]]; then
   cd $DATALGO_DIRECTORY || exit
   # Create windows for datalgo
   tmux new -s "$SESSION" -d
+  tmux send-keys -t "$SESSION" "$PYENV" C-m
   tmux new-window -t "$SESSION"
-  tmux send-keys -t "$SESSION" "$VIM" C-m
+  tmux send-keys -t "$SESSION" "$PYENV && $VIM" C-m
 else
   echo "$SESSION already exists"
 fi

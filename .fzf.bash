7c78801edd9b4f8427b8a607b9befd955fc63f60
Bradley Cushing <bradleycushing@gmail.com>
2022-01-10 20:13:48 -0500

    refactor(fzf): remove quotes around path for key bindings

--- .fzf.bash
+++ .fzf.bash
@@ -7,4 +7,4 @@ fi
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2>/dev/null
 
 # Key bindings for fzf
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+source /usr/local/opt/fzf/shell/key-bindings.bash
--- .fzf.zsh
+++ .fzf.zsh
@@ -7,4 +7,4 @@ fi
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings for fzf
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source /usr/local/opt/fzf/shell/key-bindings.zsh

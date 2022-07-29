5e3e2a971d4313297f9aff7e76eceaa2008043e5
Bradley Cushing <bradleycushing@gmail.com>
2022-07-28 20:58:21 -0400

    feat(session): add nyu tandon windows to study session

--- scripts/sessions/study.sh
+++ scripts/sessions/study.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-NYU_DIRECTORY=~/Documents/repos/nyux-cppprg1
+NYU_TANDON_DIRECTORY=~/Documents/repos/nyu-tandon-bridge
 CPPLAY_DIRECTORY=~/Documents/repos/cpplay
+NYUX_DIRECTORY=~/Documents/repos/nyux-cppprg1
 SESSION="study"
 VIM="vim"
 
@@ -9,14 +10,18 @@ tmux has-session -t "$SESSION" 2>/dev/null
 
 # An error means there is no session
 if [[ $? != 0 ]]; then
-  cd $NYU_DIRECTORY || exit
-  # Create windows for nyux
+  cd $NYU_TANDON_DIRECTORY || exit
+  # Create windows for nyu tandon
   tmux new -s "$SESSION" -d
   tmux new-window -t "$SESSION"
   tmux send-keys -t "$SESSION" "$VIM" C-m
   # Create windows for cpplay
   tmux new-window -t "$SESSION" -c $CPPLAY_DIRECTORY
   tmux new-window -t "$SESSION" -c $CPPLAY_DIRECTORY
+  tmux send-keys -t "$SESSION" "$VIM" C-m
+  # Create windows for nyux
+  tmux new-window -t "$SESSION" -c $NYUX_DIRECTORY
+  tmux new-window -t "$SESSION" -c $NYUX_DIRECTORY
   tmux send-keys -t "$SESSION" "$VIM" C-m
 else
   echo "$SESSION already exists"

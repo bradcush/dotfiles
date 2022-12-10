0745d790601d436959bae4019a4fc2dd4a8ff235
Bradley Cushing <bradleycushing@gmail.com>
2022-12-09 22:38:15 -0500

    refactor(fzf): simplify syntax for fzf bash scripts

--- .fzf.bash
+++ .fzf.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # PATH setup needed for fzf
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+if [[ $PATH != */opt/homebrew/opt/fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion fzf setup
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2>/dev/null
 
 # Key bindings for fzf
 source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"

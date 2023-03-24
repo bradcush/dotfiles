7f70cb999f500cfe708a32f72b186ebffd92e5df
Bradley Cushing <bradleycushing@gmail.com>
2023-03-23 20:49:12 -0400

    feat(type): change mapping for declaration to type definition

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -17,7 +17,7 @@ local on_attach = function(client, bufnr)
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', 'gy', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)

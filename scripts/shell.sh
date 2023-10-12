7dcd29157a90caa609c3b2f0c5318ad21a5f8f27
Bradley Cushing <bradleycushing@gmail.com>
2023-10-11 22:00:46 -0400

    feat(python): enable pyenv and virtualenv for macos

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Use vi/vim mode in the shell
 set -o vi
@@ -11,19 +11,21 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 
-# Add pyenv executable to PATH and
-# enable shims by adding the following
-# to ~/.profile and ~/.zprofile:
-# export PYENV_ROOT="$HOME/.pyenv"
-# export PATH="$PYENV_ROOT/bin:$PATH"
-# eval "$(pyenv init --path)"
+if [[ $OSTYPE == "darwin"* ]]; then
+  # Add pyenv executable to PATH and
+  # enable shims by adding the following
+  # to ~/.profile and ~/.zprofile:
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
 
-# Init for pyenv Python version manager
-# eval "$(pyenv init -)"
-# eval "$(pyenv virtualenv-init -)"
+  # Init for pyenv Python version manager
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
 
-# Hide virtual environment in prompt
-# export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+  # Hide virtual environment in prompt
+  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+fi
 
 # Launch nvim instead of vim
 VIM_EDITOR="nvim"

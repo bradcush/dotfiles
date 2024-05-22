6b845a8cad931a1bbdaaa4ec4174246c6a23b37a
Bradley Cushing <bradleycushing@gmail.com>
2024-05-22 09:43:22 -0400

    feat(nyu): update nyu session for summer courses

--- scripts/sessions/nyu.sh
+++ scripts/sessions/nyu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-NETWORKING_DIRECTORY=~/Documents/repos/csci-ga-2262
-CRYPTOGRAPHY_DIRECTORY=~/Documents/repos/csci-ga-3205
-ALGORITHMS_DIRECTORY=~/Documents/repos/csci-ga-1170
+OS_DIRECTORY=~/Documents/repos/csci-ga-2250
+MATH_DIRECTORY=~/Documents/repos/csci-ga-1180
 SESSION="study"
 VIM="vim"
 
@@ -10,18 +9,14 @@ tmux has-session -t "$SESSION" 2>/dev/null
 
 # An error means there is no session
 if [[ $? != 0 ]]; then
-  cd $NETWORKING_DIRECTORY || exit
-  # Create windows for networking
+  cd $OS_DIRECTORY || exit
+  # Create windows for operating systems
   tmux new -s "$SESSION" -d
   tmux new-window -t "$SESSION"
   tmux send-keys -t "$SESSION" "$VIM" C-m
-  # Create windows for cryptography
-  tmux new-window -t "$SESSION" -c $CRYPTOGRAPHY_DIRECTORY
-  tmux new-window -t "$SESSION" -c $CRYPTOGRAPHY_DIRECTORY
-  tmux send-keys -t "$SESSION" "$VIM" C-m
-  # Create windows for algorithms
-  tmux new-window -t "$SESSION" -c $ALGORITHMS_DIRECTORY
-  tmux new-window -t "$SESSION" -c $ALGORITHMS_DIRECTORY
+  # Create windows for mathematical techniques
+  tmux new-window -t "$SESSION" -c $MATH_DIRECTORY
+  tmux new-window -t "$SESSION" -c $MATH_DIRECTORY
   tmux send-keys -t "$SESSION" "$VIM" C-m
 else
   echo "$SESSION already exists"

912e97f63dce954958bf214e6fa7d605ec6690e5
Bradley Cushing <bradleycushing@gmail.com>
2024-09-07 15:15:10 -0400

    feat(nyu): update nyu classes for tmux session

--- scripts/sessions/nyu.sh
+++ scripts/sessions/nyu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-OS_DIRECTORY=~/Documents/repos/csci-ga-2250
-MATH_DIRECTORY=~/Documents/repos/csci-ga-1180
+DS_DIRECTORY=~/Documents/repos/csci-ga-2433
+CRYPTO_DIRECTORY=~/Documents/repos/csci-ga-3210
 SESSION="study"
 VIM="vim"
 
@@ -9,14 +9,14 @@ tmux has-session -t "$SESSION" 2>/dev/null
 
 # An error means there is no session
 if [[ $? != 0 ]]; then
-  cd $OS_DIRECTORY || exit
+  cd $DS_DIRECTORY || exit
   # Create windows for operating systems
   tmux new -s "$SESSION" -d
   tmux new-window -t "$SESSION"
   tmux send-keys -t "$SESSION" "$VIM" C-m
   # Create windows for mathematical techniques
-  tmux new-window -t "$SESSION" -c $MATH_DIRECTORY
-  tmux new-window -t "$SESSION" -c $MATH_DIRECTORY
+  tmux new-window -t "$SESSION" -c $CRYPTO_DIRECTORY
+  tmux new-window -t "$SESSION" -c $CRYPTO_DIRECTORY
   tmux send-keys -t "$SESSION" "$VIM" C-m
 else
   echo "$SESSION already exists"

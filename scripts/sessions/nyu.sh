21e50163aefe0d7f710b25308f270f66cf147e1f
Bradley Cushing <bradleycushing@gmail.com>
2025-05-26 21:13:40 -0400

    feat(nyu): update nyu sessions for spring semester

--- scripts/sessions/nyu.sh
+++ scripts/sessions/nyu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DS_DIRECTORY=~/Documents/repos/csci-ga-2433
-CRYPTO_DIRECTORY=~/Documents/repos/csci-ga-3210
+COMPUTATION_DIRECTORY=~/Documents/repos/csci-ua-0453
+CRYPTO_BLOCKCHAINS_DIRECTORY=~/Documents/repos/csci-ga-3033-107
+CRYPTO_RESEARCH_DIRECTORY=~/Documents/repos/csci-ga-3813
 SESSION="study"
 VIM="vim"
 
@@ -9,14 +10,18 @@ tmux has-session -t "$SESSION" 2>/dev/null
 
 # An error means there is no session
 if [[ $? != 0 ]]; then
-  cd $DS_DIRECTORY || exit
-  # Create windows for operating systems
+  cd $COMPUTATION_DIRECTORY || exit
+  # Create windows for computation
   tmux new -s "$SESSION" -d
   tmux new-window -t "$SESSION"
   tmux send-keys -t "$SESSION" "$VIM" C-m
-  # Create windows for mathematical techniques
-  tmux new-window -t "$SESSION" -c $CRYPTO_DIRECTORY
-  tmux new-window -t "$SESSION" -c $CRYPTO_DIRECTORY
+  # Create windows for cryptography of blockchains
+  tmux new-window -t "$SESSION" -c $CRYPTO_BLOCKCHAINS_DIRECTORY
+  tmux new-window -t "$SESSION" -c $CRYPTO_BLOCKCHAINS_DIRECTORY
+  tmux send-keys -t "$SESSION" "$VIM" C-m
+  # Create windows for cryptography research
+  tmux new-window -t "$SESSION" -c $CRYPTO_RESEARCH_DIRECTORY
+  tmux new-window -t "$SESSION" -c $CRYPTO_RESEARCH_DIRECTORY
   tmux send-keys -t "$SESSION" "$VIM" C-m
 else
   echo "$SESSION already exists"

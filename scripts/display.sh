2e74cda9bf550cfdb70c2e44007e452ff33f269e
Bradley Cushing <bradleycushing@gmail.com>
2025-07-17 10:46:06 -0400

    feat(display): make lg monitor the default

--- scripts/display.sh
+++ scripts/display.sh
@@ -2,7 +2,7 @@
 
 # Preferred settings using an additional LG monitor on the
 # right side and the builtin laptop screen on the left.
-# xrandr --output eDP1 --auto --output DP3 --mode 2560x1440 --right-of eDP1
+xrandr --output eDP1 --auto --output DP3 --mode 2560x1440 --right-of eDP1
 
 # Reset back to single laptop monitor
 # xrandr --output eDP1 --auto --output DP3 --off
